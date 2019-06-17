cb16f08bb396bbf9a718ed4f2fe5c7bc4e0f8653
Joseph Escobedo <josephescobedo@Josephs-MacBook-Pro.local>
2019-06-16 22:31:01 -0700

    found way to load the right color options based on shirt design, now have to find a way to hide cornflower blue if heart js is clicked as the design option.

--- js/app.js
+++ js/app.js
@@ -18,12 +18,28 @@ $("#removeOption:selected").css("display", "none");
 $("#design").on("change", e => {
   //need to find a way to make sure first click is one or the other design
   if (e.target.value === "js puns") {
-    // $("#color:selected").val([]);
     console.log("jspuns");
     //load correct color for js puns
+    for (let i = 0; i < colorChildren.length; i++) {
+      if (i <= 2) {
+        $(colorChildren[i]).show();
+      }
+      if (i > 2) {
+        $(colorChildren[i]).hide();
+      }
+    }
   }
   if (e.target.value === "heart js") {
+    //find way to make Cornflower Blue for JS PUNS options go away if heart js is clicked as the design option.
     console.log("heart js");
     //load correct color for heart js
+    for (let i = 0; i < colorChildren.length; i++) {
+      if (i > 2) {
+        $(colorChildren[i]).show();
+      }
+      if (i <= 2) {
+        $(colorChildren[i]).hide();
+      }
+    }
   }
 });
